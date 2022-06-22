41d03a51bd29c0bdb9057998eec845bb82831522
Augusto Cesar Boebel <augusto.boebel@alunos.sc.senac.br>
2022-06-22 16:05:59 -0300

    primeiro commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+include "nav.php";
+?>
+
+
+
+<div class="titulotexto">
+    <h1>Tesla</h1>
+</div>
+
+
+
+
---
+++ nav.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tesla.exe</title>
+    <link rel="shortcut icon" href="assets/images/logo.png">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+</head>
+
+
+<body>
+
+    <div class="cabeçalho">
+        <header>
+            <nav>
+                <div class="nav-container">
+                    <a href="sobre.html">
+                        <img src="assets/images/logo.png" alt="Logo do site" class="logo">
+
+                    </a>
+
+                    <ul>
+                        <li><a href="#">Suporte</a></li>
+                        <li><a href="#">Catálogo</a></li>
+                        <li><a href="#"> Sobre nós</a></li>
+                    </ul>
+                </div>
+        </header>
+
+        </nav>
+
+        <div id='busca'>
+            <form action="/search" class="search" method="get">
+                <input id="txtbusca" name="q" type="text" value="" placeholder="O que procura?" />
+                <input id="btnBusca" type="submit" value="search" />
+            </form>
+        </div>
+    </div>
+
+
+
+
+
+
+
+</body>
+
